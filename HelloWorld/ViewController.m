f08b95687d556f762fc25026b2c1f28204325303
Nikhil Shah <tricalnikhil@gmail.com>
2014-07-22 23:10:06 -0700

    More changes to VC

--- HelloWorld/ViewController.m
+++ HelloWorld/ViewController.m
@@ -19,7 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    [self.view setBackgroundColor:[UIColor whiteColor]];
+    [self.view setBackgroundColor:[UIColor redColor]];
 }
 
 - (void)didReceiveMemoryWarning
